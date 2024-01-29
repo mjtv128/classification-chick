628a443ddecf6aaf931ea661e45a0aa053013080
mjv27 <michelle.van@duke.edu>
2024-01-29 00:08:42 -0800

    added setup

--- template.py
+++ template.py
@@ -21,7 +21,8 @@ file_list = [
   'params.yaml',
   'requirements.txt',
   'setup.py',
-  'research/trials.ipynb'  
+  'research/trials.ipynb',
+  'templates/index.html'
 ]
 
 for filepath in file_list:
@@ -30,7 +31,7 @@ for filepath in file_list:
   
   if filedir != "":
     os.makedirs(filedir, exist_ok=True)
-    logging.info(f"Creating directory; {filedir} for the file: {filename}")
+    logging.info(f"Creating directory: {filedir} for the file: {filename}")
     
   if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
     with open(filepath, 'w') as f:

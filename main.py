13c95863f9491aa2204f1d6b7f580a40d50e27b2
mjv27 <michelle.van@duke.edu>
2024-01-29 00:52:25 -0800

    added logger

---
+++ main.py
@@ -0,0 +1,9 @@
+# from src.cnnClassifier import logger
+import sys
+import os
+
+from src.cnnClassifier.utils import logger
+
+# sys.path.append('src.cnnClassifier')
+# from cnnClassifier import logger
+logger.info("Welcome to my custom log")
